556736caf554741a9e8738ae16276b06874e9776
TimBdk <timbdk@gmail.com>
2026-04-26 03:33:29 +0200

    fix(event-signing-service): pass empty array to NDKNip46Backend constructor so its `switch_relays` command does not pass on its internal `ws://relay:7777` address to the web-client.

--- src/daemon/backend/index.ts
+++ src/daemon/backend/index.ts
@@ -4,6 +4,6 @@ import { IConfig } from '../../config/index.js'
 export class Backend extends NDKNip46Backend {
   constructor(ndk: NDK, key: string, cb: Nip46PermitCallback, config: IConfig) {
     const signer = new NDKPrivateKeySigner(key)
-    super(ndk, signer, cb)
+    super(ndk, signer, cb, [])
   }
 }
